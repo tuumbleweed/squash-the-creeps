9867f90f0a86a3ee57687ce9396b5de323452c88
Tumbleweed <tuumbleweed@users.noreply.github.com>
2025-09-21 09:09:23 -0500

    Add interface

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$UserInterface/Retry.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,6 +24,9 @@ func _on_mob_timer_timeout():
 
 	var player_position = $Player.position
 	mob.initialize(mob_spawn_location.position, player_position)
+	
+	# We connect the mob to the score label to update the score upon squashing one.
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
@@ -31,3 +34,10 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit() -> void:
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
